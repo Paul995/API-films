150f831e2b0e98939ef800c610680ca592c4506e
Your Name <pwdawson90@gmail.com>
2024-02-13 15:04:06 -0500

    final1

--- app.js
+++ app.js
@@ -55,33 +55,43 @@ server.post("/utilisateurs/initialiser", (req, res) => {
 
 
 
+//////////////////////////////////////////////////////////////////
+
+
 //----GET **********************
 //points dacces      // ceci rajoute la possibilite de async
-server.get("/films", async (req, res) => {
+server.get("/api/films", [
+  //  selection / conv unicode(protction xss) / espaces / pour eviter de creer champs vide des autres values / impossible de creer champ vide
+    check('tri').escape().trim().notEmpty()
+  ], async (req, res) => {
   //    const test = {email: "sdjkfh@srjfghdg.com"};
   try {
-    console.log(req.query); //ne fait rien mais permet de filtrer db pour voir/test avec lurl
+    console.trace(req.query); //ne fait rien mais permet de filtrer db pour voir/test avec lurl
     // on peut rajouer dans lurl ex: films/?limit=2&direction = asc
+
+
+    const tri = req.query.tri || "titre";
 
     const direction = req.query["order-direction"] || "asc";
     const limit = +req.query["limit"] || 5; // OU nouveau shit :  +req.query("limit");
     // references firestore (car nest pas encore json)
 
-    //2 premiers els
-    const donneesRef = await db
-      .collection("films")
-      .orderBy("titre", direction)
-      .limit(limit)
-      .get();
-    const donneesFinale = [];
+    
+    if(tri == "titre" || tri == "annee" || tri == "realisation"){
+      const donneesRef = await db.collection("films").orderBy(tri, direction).limit(limit).get();
 
-    donneesRef.forEach((doc) => {
-      donneesFinale.push(doc.data());
-    });
+      //2 premiers els
 
-    res.statusCode = 200; //comme if
-    res.json(donneesFinale); 
-  } catch (e) {
+      const donneesFinale = [];
+
+      donneesRef.forEach((doc) => {
+        donneesFinale.push(doc.data());
+      });
+
+      res.statusCode = 200; //comme if
+      res.json(donneesFinale); 
+    } 
+  }catch (e) {
     res.statusCode = 500;
 
     res.json({ message: "erreur type 500" });
@@ -90,7 +100,10 @@ server.get("/films", async (req, res) => {
 
 
 //autre route    param dynamique!(:id)   params = tout les shits avec /:...
-server.get("/films/:id", async (req, res) => {
+server.get("/api/films/:id", [
+  //  selection / conv unicode(protction xss) / espaces / pour eviter de creer champs vide des autres values / impossible de creer champ vide
+    check('id').escape().trim().notEmpty()
+  ], async (req, res) => {
     const id = req.params.id;
     const donneesRef = await db.collection("films").doc(id).get();
     const film = donneesRef.data()
@@ -98,20 +111,68 @@ server.get("/films/:id", async (req, res) => {
     res.json(film);
 });
 
-server.get("/utilisateurs/:id", async (req, res) => {
-    const id = req.params.id;
-    const donneesRef = await db.collection("utilisateurs").doc(id).get();
-    const user = donneesRef.data()
-    res.statusCode = 200;
-    res.json(user);
-});
+
+//UPDATE  (PUT)
+server.put("/api/films/:id", [
+//  selection / conv unicode(protction xss) / espaces / pour eviter de creer champs vide des autres values / impossible de creer champ vide
+  check('annee').escape().trim().optional().notEmpty(),
+  check('description').escape().trim().optional().notEmpty(),
+  check('genres').escape().trim().optional().notEmpty().isArray(),
+  check('realisation').escape().trim().optional().notEmpty(),
+  check('titre').escape().trim().optional().notEmpty(),
+  check('titreVignette').escape().trim().optional().notEmpty()
+], async (req, res) => {
+  const id = req.params.id;
+  const filmsModifies = req.body; //le corps de la nouvelle requete envoye (obj)
+  //validation ici
+console.log(filmsModifies);
+
+  await db.collection("films").doc(id).update(filmsModifies);
+
+  res.statusCode = 200;
+  res.json({message:"le film a ete modifie"});
+})
   
+
+// POST   *********************
+server.post("/api/films", [
   
+  check('annee').escape().trim().optional().notEmpty(),
+  check('description').escape().trim().optional().notEmpty(),
+  check('genres').escape().trim().optional().notEmpty().isArray(),
+  check('realisation').escape().trim().optional().notEmpty(),
+  check('titre').escape().trim().optional().notEmpty(),
+  check('titreVignette').escape().trim().optional().notEmpty()
+], async (req, res) => {
+  try{
+    const test = req.body;
+
+    // validation des donnees
+    if(test.films == undefined){
+      res.statusCode = 400;
+      //retrun fait sortir de la fctn. sinon erreur
+      return res.json({message: "vous devez fournir un film"});
+    }
+                          // add() fct firestore pour ajouter de la donnee
+    await(db.collection("films").add(test));
+
+    res.statusCode = 201;
+    res.json({message: "la donne a ete ajoute", donnees: test});
+  }catch{
+    res.statusCode = 500;
+    res.json({message: "erreur 500"});
+  }
+
+})
+
 
 
 // DELETE ******************
 //delete film
-server.delete("/films/:id", async (req, res) => {
+server.delete("/api/films/:id",[
+  //  selection / conv unicode(protction xss) / espaces / pour eviter de creer champs vide des autres values / impossible de creer champ vide
+    check('id').escape().trim().notEmpty()
+  ], async (req, res) => {
     const id = req.params.id;
     const resultat = await db.collection("films").doc(id).delete();
   
@@ -119,15 +180,97 @@ server.delete("/films/:id", async (req, res) => {
     res.json({message: "film deleted"})
   
   })
-//delete user
-server.delete("/utilisateurs/:id", async (req, res) => {
-    const id = req.params.id;
-    const resultat = await db.collection("utilisateurs").doc(id).delete();
-  
-    res.statusCode = 200;
-    res.json({message: "user deleted"})
 
+
+  //////////////////////////   USERS   //////////////////
+
+   //validation!!
+                                  //valide couriel (syntaxe express-validator)
+server.post("/utilisateurs/inscription",[
+  check("courriel").escape().trim().notEmpty().isEmail().normalizeEmail(),
+  check("mdp").escape().trim().notEmpty().isLength({min:8,max:20}).isStrongPassword({
+    minLength:8,
+    minLowercase:1,
+    minNumbers:1,
+    minUppercase:1,
+    minSymbols:1
   })
+
+//      req deja netoyee
+], async (req, res) => {
+  //validator  si erreur de validation
+  const validation = validationResult(req);
+        if (validation.errors.length > 0) {
+            res.statusCode = 400;
+            return res.json({ message: "Données non-conformes" });
+        }
+
+  // On récupère les infos du body
+  const {courriel, mdp} = req.body; //destructuration// je vais dans body et je sort courreil et mdp. = const courriel = req.body.courriel etc
+
+  // On vérifie si le courriel existe
+  const docRef = await db.collection("utilisateurs").where("courriel", "==", courriel).get();
+  const utilisateurs = [];
+  
+  docRef.forEach((doc) => {
+    utilisateurs.push(doc.data());
+  });
+  console.log(utilisateurs);
+  // Si oui, erreur
+  if (utilisateurs.length > 0){
+    res.statusCode = 400;
+    res.json({message : "le courriel existe deja"})
+  }
+
+  // On valide/nettoie la donnée
+  // On encrypte le mot de passe
+  // On enregistre (objet)
+  const nouvelUtilisateur = { // quand key et value dans objet sont pareils (et value est var), on peut juste ecrire le  value
+    "courriel":courriel, 
+    "mdp": mdp
+  };
+  await db.collection("utilisateurs").add(nouvelUtilisateur); // on peut enlever le mdp de l"objet (juste pour le retour), pour pas montrer le mdp, meme si il est enregistre dans la db
+  delete nouvelUtilisateur.mdp;
+
+  // On renvoie true;
+  res.json(nouvelUtilisateur);
+});
+
+
+
+
+  
+server.post("/utilisateurs/connexion", async (req, res) => {
+  // On récupère les infos du body
+  const { mdp, courriel } = req.body;
+
+  // On vérifie si le courriel existe
+  const docRef = await  db.collection("utilisateurs").where("courriel", "==", courriel).get();
+  const utilisateurs = [];
+  docRef.forEach((utilisateur) => {
+    utilisateurs.push(utilisateur.data());
+  })
+  // Si non, erreur
+  if(utilisateurs.length == 0){
+    res.statusCode = 400;
+    return res.json({message:"courriel invalide"})
+  };
+  const  utilisateurAValider = utilisateurs[0];
+  // TODO: On encrypte le mot de passe
+  // On compare
+    // si pas pareil, erreur
+  if (utilisateurAValider.mdp !== mdp) {
+    res.statusCode = 400;
+    res.json({ message: "Mot de passe invalide" });
+}
+
+  // On retourne les infos de l'utilisateur sans le mot de passe
+  delete utilisateurAValider.mdp;
+  res.status = 200;
+  res.json(utilisateurAValider);
+});
+
+ 
 
 
 
